5628ad3bbff394e501bd3fdbbb6c1c1b210b6031
Michael Sevestre <michael.sevestre@gmail.com>
2017-08-01 12:36:28 -0400

    Remove all js from home.html

--- js/site.js
+++ js/site.js
@@ -3,20 +3,44 @@
       $('ul.toggle').slideToggle(600)	
     });	
     
-    $(document).ready(function() {
-      $(window).bind('scroll', function() {
-        if ($(window).scrollTop() > 0) {
-          $('#header_outer').addClass('fixed');
-        }
-        else {
-          $('#header_outer').removeClass('fixed');
-        }
-      });
-      
+    $('ul.toggle a').click(function(){
+      $('ul.toggle').slideToggle(600)	
+    });	
+    
+    $(window).bind('scroll', function() {
+      if ($(window).scrollTop() > 0) {
+        $('#header_outer').addClass('fixed');
+      }
+      else {
+        $('#header_outer').removeClass('fixed');
+      }
     });
     
     
+    $('#header_outer').scrollToFixed();
+    $('.res-nav_click').click(function(){
+      $('.main-nav').slideToggle();
+      return false    
+      
+    });
+    
   });	
+  
+  $(window).load(function(){
+    
+    $('a').bind('click',function(event){
+      var $anchor = $(this);
+      
+      var hrefOffset = $($anchor.attr('href')).offset();
+      if(hrefOffset){
+        $('html, body').stop().animate({
+          scrollTop: hrefOffset.top - 91
+        }, 1500,'easeInOutExpo');
+      }
+      event.preventDefault();
+    });
+  })
+  
   function resizeText() {
     var preferredWidth = 767;
     var displayWidth = window.innerWidth;
