9593395984c1f827bc979167ce53e04caf368983
Michael Sevestre <michael.sevestre@gmail.com>
2017-08-01 12:41:59 -0400

    Remove all js from home.html

--- js/site.js
+++ js/site.js
@@ -1,52 +1,50 @@
-	$(document).ready(function(e) {
-    $('.res-nav_click').click(function(){
-      $('ul.toggle').slideToggle(600)	
-    });	
-    
-    $('ul.toggle a').click(function(){
-      $('ul.toggle').slideToggle(600)	
-    });	
-    
-    $(window).bind('scroll', function() {
-      if ($(window).scrollTop() > 0) {
-        $('#header_outer').addClass('fixed');
-      }
-      else {
-        $('#header_outer').removeClass('fixed');
-      }
-    });
-    
-    
-    $('#header_outer').scrollToFixed();
-    $('.res-nav_click').click(function(){
-      $('.main-nav').slideToggle();
-      return false    
-      
-    });
-    
+wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:       100
+  }
+);
+wow.init();
+
+$(document).ready(function(e) {
+  $('.res-nav_click').click(function(){
+    $('ul.toggle').slideToggle(600)	
   });	
   
-  $(window).load(function(){
+  $('ul.toggle a').click(function(){
+    $('ul.toggle').slideToggle(600)	
+  });	
+  
+  $(window).bind('scroll', function() {
+    if ($(window).scrollTop() > 0) {
+      $('#header_outer').addClass('fixed');
+    }
+    else {
+      $('#header_outer').removeClass('fixed');
+    }
+  });
+  
+  
+  $('#header_outer').scrollToFixed();
+  $('.res-nav_click').click(function(){
+    $('.main-nav').slideToggle();
+    return false    
     
-    $('a').bind('click',function(event){
-      var $anchor = $(this);
-      
-      var hrefOffset = $($anchor.attr('href')).offset();
-      if(hrefOffset){
-        $('html, body').stop().animate({
-          scrollTop: hrefOffset.top - 91
-        }, 1500,'easeInOutExpo');
-      }
-      event.preventDefault();
-    });
-  })
+  });
   
-  function resizeText() {
-    var preferredWidth = 767;
-    var displayWidth = window.innerWidth;
-    var percentage = displayWidth / preferredWidth;
-    var fontsizetitle = 25;
-    var newFontSizeTitle = Math.floor(fontsizetitle * percentage);
-    $(".divclass").css("font-size", newFontSizeTitle)
-  }
+});	
+
+$(window).load(function(){
   
+  $('a').bind('click',function(event){
+    var $anchor = $(this);
+    
+    var hrefOffset = $($anchor.attr('href')).offset();
+    if(hrefOffset){
+      $('html, body').stop().animate({
+        scrollTop: hrefOffset.top - 91
+      }, 1500,'easeInOutExpo');
+    }
+    event.preventDefault();
+  });
+})
