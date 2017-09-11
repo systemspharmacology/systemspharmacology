e39150d68a6c96c20548555a816d7aa4be479324
Michael Sevestre <michael@design2code.ca>
2017-09-11 14:55:35 -0400

    Designer changes (#4)
    * First changes according to designer feedback
    
    * Some more work for better navigation
    
    * Update for designer changes

--- _tutorials/05-comparison.md
+++ _tutorials/05-comparison.md
@@ -2,5 +2,5 @@
 title:  "Comparing building blocks and simulations in PK-Sim® and MoBi®"
 description: "Finding differences between a template building block and a building block used in a simulation, or comparing two simulations is an essential tool to understand how a model is built. This tutorial explains how to perform such a comparison in PK-Sim® and MoBi®" 
 full_url: "http://www.systems-biology.com/uploads/pics/Comparison_01.mp4"
-icon: film  
+icon: play  
 ---
--- js/site.js
+++ js/site.js
@@ -6,45 +6,23 @@ wow = new WOW(
 );
 wow.init();
 
-$(document).ready(function(e) {
-  $('.res-nav_click').click(function(){
-    $('ul.toggle').slideToggle(600)	
-  });	
-  
-  $('ul.toggle a').click(function(){
-    $('ul.toggle').slideToggle(600)	
-  });	
-  
-  $(window).bind('scroll', function() {
-    if ($(window).scrollTop() > 0) {
-      $('.header').addClass('fixed');
-    }
-    else {
-      $('.header').removeClass('fixed');
-    }
-  });
-  
-  
-  $('#header').scrollToFixed();
-  $('.res-nav_click').click(function(){
-    $('.main-nav').slideToggle();
-    return false    
-    
-  });
-  
-});	
-
-$(window).load(function(){
-  
-  $('a').bind('click',function(event){
-    var $anchor = $(this);
-    
-    var hrefOffset = $($anchor.attr('href')).offset();
-    if(hrefOffset){
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
       $('html, body').stop().animate({
-        scrollTop: hrefOffset.top - 91
-      }, 1500,'easeInOutExpo');
-    }
-    event.preventDefault();
+          scrollTop: $($anchor.attr('href')).offset().top 
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
   });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+  target: '.navbar-fixed-top'
 })
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $('.navbar-toggle:visible').click();
+});
