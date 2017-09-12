55a2fe742f36de642ae21adc7d5922e3ceabf733
Michael Sevestre <michael.sevestre@gmail.com>
2017-09-11 20:59:56 -0400

    Some minor changes to scrolling

--- js/site.js
+++ js/site.js
@@ -8,19 +8,23 @@ wow.init();
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+
+  var navOffset = $('#navbar').height();
+  console.log(navOffset);
   $('a.page-scroll').bind('click', function(event) {
       var $anchor = $(this);
       $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top 
+          scrollTop: $($anchor.attr('href')).offset().top - navOffset
       }, 1500, 'easeInOutExpo');
       event.preventDefault();
   });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-  target: '.navbar-fixed-top'
-})
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: navOffset + 5
+  })
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
